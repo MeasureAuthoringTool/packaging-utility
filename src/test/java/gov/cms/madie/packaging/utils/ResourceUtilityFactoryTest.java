6ee66e8e35ecc974ee66d9cc32398ac6ff99fa29
nmorasb <nate.moraca@semanticbits.com>
2026-07-29 17:38:51 -0400

    MAT-10307: add new packagingutility implementation for USQC, and required mapping

--- src/test/java/gov/cms/madie/packaging/utils/ResourceUtilityFactoryTest.java
+++ src/test/java/gov/cms/madie/packaging/utils/ResourceUtilityFactoryTest.java
@@ -2,6 +2,7 @@ package gov.cms.madie.packaging.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import gov.cms.madie.models.common.ModelType;
 import java.lang.reflect.InvocationTargetException;
 
 import org.junit.jupiter.api.Test;
@@ -48,6 +49,26 @@ class ResourceUtilityFactoryTest {
       PackagingUtility utility = PackagingUtilityFactory.getInstance("QI-Core v6.0.0");
       assertNotNull(utility);
 
+    } catch (InstantiationException
+        | IllegalAccessException
+        | IllegalArgumentException
+        | InvocationTargetException
+        | NoSuchMethodException
+        | SecurityException
+        | ClassNotFoundException e) {
+      fail(e);
+    }
+  }
+
+  @Test
+  void testGetUsQualityCoreInstance() {
+    try {
+      PackagingUtility utility =
+          PackagingUtilityFactory.getInstance(ModelType.US_QUALITY_CORE_0_5_0.getValue());
+
+      assertInstanceOf(
+          gov.cms.madie.packaging.utils.usqualitycore.PackagingUtilityImpl.class, utility);
+      assertInstanceOf(gov.cms.madie.packaging.utils.qicore411.PackagingUtilityImpl.class, utility);
     } catch (InstantiationException
         | IllegalAccessException
         | IllegalArgumentException
